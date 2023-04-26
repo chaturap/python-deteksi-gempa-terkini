078b358e562c723235fc86caa38fd2a9353b6804
chaturap <catur.ap@gmail.com>
2023-04-26 17:38:07 +0700

    tambah spasi

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ Aplikasi deteksi gempa , data source dari BMKG
 from gempaterkini import tampilkan_data, ekstrasi_data
 
 if __name__ == '__main__':
-    print("Aplikasi Utama")
+    print("Aplikasi  Utama")
     result = ekstrasi_data()
     tampilkan_data(result)
